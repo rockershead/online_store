a1342883be9d5598995545eca7cf45ba59abb9f0
rockershead <zahir.inspireme@gmail.com>
2023-11-25 17:46:58 +0800

    added clear cart endpoint

--- e-commerce-api/server/controllers/shopping_cart/clearCart.js
+++ e-commerce-api/server/controllers/shopping_cart/clearCart.js
@@ -1,17 +1,14 @@
 const { redisClient } = require("../../utils");
 
-const deleteItem = () => async (req, res, next) => {
+const clearCart = () => async (req, res, next) => {
   //get the userId and set that id to the products
   const decodedToken = res.locals.result;
   const email = decodedToken.email;
-  const { productId } = req.params;
 
   const cart = await redisClient.get(email);
-  const products = JSON.parse(cart);
-  if (cart && products.length > 0) {
-    const updatedArr = products.filter((obj) => obj.productId !== productId);
 
-    await redisClient.set(email, JSON.stringify(updatedArr));
+  if (cart) {
+    await redisClient.set(email, JSON.stringify([]));
     const result = await redisClient.get(email);
     res.status(200).send(`${result}`);
   } else {
@@ -19,4 +16,4 @@ const deleteItem = () => async (req, res, next) => {
   }
 };
 
-module.exports = { deleteItem };
+module.exports = { clearCart };
--- e-commerce-api/server/controllers/shopping_cart/index.js
+++ e-commerce-api/server/controllers/shopping_cart/index.js
@@ -5,11 +5,13 @@ const { list } = require("./list");
 const { deleteItem } = require("./deleteItem");
 const { showItem } = require("./showItem");
 const { updateQuantity } = require("./updateQuantity");
+const { clearCart } = require("./clearCart");
 
 module.exports = () => {
   const api = router();
 
   api.post("/", addItem());
+  api.post("/clear_cart", clearCart());
   api.post("/update_quantity", updateQuantity());
   api.get("/", list());
   api.delete("/:productId", deleteItem());
